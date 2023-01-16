7ff50ef97946000c16351aafcd4f4bd1b152e819
Parkash Heerani <50080906+parkashheerani@users.noreply.github.com>
2023-01-16 13:46:19 -0500

    Add files via upload

---
+++ answer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 16 13:45:19 2023
+
+@author: parkashheerani
+"""
+
+def sum(a, b):
+    return a + b
+
+def product(a, b):
+    return a * b
